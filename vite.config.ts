f7decc511a1e0db6f2eaf78db212cece0789bc69
SebastianHinestroza12 <menas7527@gmail.com>
2024-06-14 18:03:28 -0500

    fix(vite.config): Correction in PWA configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,42 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-// import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
     react(),
-    // VitePWA({
-    //   manifest: {
-    //     name: 'CrypWallet',
-    //     short_name: 'CW',
-    //     description: 'Billetera de criptomonedas',
-    //     theme_color: '#19223c',
-    //     background_color: '#000000',
-    //     display: 'standalone',
-    //     display_override: ['window-controls-overlay'],
-    //     lang: 'en',
-    //     icons: [
-    //       {
-    //         src: '/public/icon-64x64.png',
-    //         sizes: '64x64',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: '/public/icon-192x192.png',
-    //         sizes: '192x192',
-    //         type: 'image/png',
-    //         // purpose: 'any',
-    //       },
-    //       {
-    //         src: '/public/icon-512x512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //         // purpose: 'maskable',
-    //       },
-    //     ],
-    //   },
-    //   registerType: 'autoUpdate',
-    // }),
+    VitePWA({
+      manifest: {
+        name: 'CrypWallet',
+        short_name: 'CW',
+        description: 'Billetera de criptomonedas',
+        theme_color: '#006400',
+        background_color: '#000000',
+        display: 'standalone',
+        display_override: ['window-controls-overlay'],
+        lang: 'en',
+        icons: [
+          {
+            src: 'icon-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+      registerType: 'autoUpdate',
+    }),
   ],
   server: {
     host: true,
