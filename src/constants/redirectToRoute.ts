a1e556657b277d820e8ef06d3fecf35fdc6b9e03
SebastianHinestroza12 <menas7527@gmail.com>
2024-07-05 19:01:51 -0500

    feat: Component and service for the creation of wallets

---
+++ src/constants/redirectToRoute.ts
@@ -0,0 +1,28 @@
+type Routes = {
+  [key: string]: string;
+};
+
+export const ROUTES: Routes = {
+  ROOT: '/',
+  HOME: '/home',
+  ABOUT_US: '/about-us',
+  NOTIFICATIONS: '/notifications',
+  WALLETS: '/wallets',
+  CREATE_WALLET: '/wallets/created',
+  PREFERENCES: '/preferences',
+  PREFERENCES_LIST: '/preferences/list',
+  PREFERENCES_CURRENCY: '/preferences/list/currency',
+  PREFERENCES_LANGUAGE: '/preferences/list/app-language',
+  AUTHENTICATION: '/auth',
+  USER_SIGNIN: '/auth/user-signin',
+  USER_SIGNUP: '/auth/user-signup',
+  USER_PROFILE: '/auth/user-profile',
+  RECOVER_ACCOUNT: '/auth/recover-account',
+  CRYPTOCURRENCY: '/crypto',
+  CRYPTO_DETAIL_MAIN: '/crypto/detail',
+  CRYPTO_DETAIL: '/crypto/detail/:cryptoId',
+  CRYPTO_MANAGE: '/crypto/manage',
+  CRYPTO_SEARCH: '/crypto/search',
+  NOT_FOUND: '*',
+  EMPTY: '',
+};
