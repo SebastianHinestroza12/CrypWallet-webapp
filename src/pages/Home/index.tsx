c3cd0937e2917e53aa92da47094e3e0c34aeb94e
SebastianHinestroza12 <menas7527@gmail.com>
2024-07-09 10:59:04 -0500

    refactor: Moved the total props component logic to a local scope

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,33 +1,17 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import { Box, Flex, Stack, Text, useColorModeValue } from '@chakra-ui/react';
 import { TotalCash } from '../../components/TotalCash';
 import { OperationButton } from '../../components/OperationButton';
 import { ListCryptocurrencies } from '../../components/ListCryptocurrencies';
 import { OPERATION_BUTTONS, ROUTES } from '../../constants';
-import { useStoreCrypto } from '../../stores/cryptocurrencies';
 import { Link } from 'react-router-dom';
-import { fetchCryptoCompareData } from '../../utils';
 
 export const Home: FC = () => {
   const BG_COLOR = useColorModeValue('gray.100', '#171717');
-  const { currency, setCurrentCrypto } = useStoreCrypto();
-  const [isLoading, setIsLoading] = useState(false);
-
-  const onRefresh = async () => {
-    try {
-      setIsLoading(true);
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-      const data = await fetchCryptoCompareData(currency);
-      setCurrentCrypto(data);
-      setIsLoading(false);
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
   return (
     <Stack spacing={5}>
-      <TotalCash isPositive percentage="5.57%" onRefresh={onRefresh} isLoading={isLoading} />
+      <TotalCash />
       <Flex justifyContent="space-between">
         {OPERATION_BUTTONS.map((button) => (
           <OperationButton key={button.text} icon={button.icon} text={button.text} />
